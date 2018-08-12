7a6f4ebe94d971e3baad02c3858c85b19e1fdf6e
fangwei <979629903@qq.com>
2018-08-13 00:13:18 +0800

    制定Java版本

--- src/main/java/com/chinawyny/generic/Generic.java
+++ src/main/java/com/chinawyny/generic/Generic.java
@@ -1,8 +1,34 @@
 package com.chinawyny.generic;
 
-public class Generic {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-    public <T> T getParam(){
-        return null;
+public class Generic<T, U> {
+
+    public <T> List<T> getParam(){
+        return new ArrayList<T>();
+    }
+
+    public static void main(String[] args) {
+        List<String> aList = Arrays.asList("111","222");
+        List<? extends Object> covariantList = aList;
+        List<? super String> contravariantList = aList;
+//        covariantList.add("d"); //wrong
+        Object a = covariantList.get(0);
+        System.out.println(a);
+        contravariantList.add(1, "d"); //OK
+//        String b = contravariantList.get(1); //wrong
+        Object c = contravariantList.get(1);}
+}
+class Super {
+    void getSomething(String str){
+        System.out.println("2");
+    }
+}
+class Sub extends Super {
+
+    void getSomething(Object str) {
+        System.out.println(str);
     }
 }
---
+++ src/main/java/com/chinawyny/generic/Node.java
@@ -0,0 +1,29 @@
+package com.chinawyny.generic;
+
+public class Node<T> {
+    public T data;
+
+    public Node(T data) { this.data = data; }
+
+    public void setData(T data) {
+        System.out.println("Node.setData");
+        this.data = data;
+    }
+
+    public static void main(String[] args) {
+
+        MyNode mn = new MyNode(2);
+        Node n =mn;
+        n.setData("Hello");
+        Integer x =mn.data;
+    }
+}
+
+class MyNode extends Node<Integer>{
+    public MyNode(Integer data) { super(data);}
+
+    public void setData(Integer data) {
+        System.out.println("MyNode.setData");
+        super.setData(data);
+    }
+}

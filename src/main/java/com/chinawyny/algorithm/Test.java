46dde92ba71341b68fa1be82d4776e1c90ca75d5
fangwei0107 <979629903@qq.com>
2018-08-19 10:17:01 +0800

    算法

---
+++ src/main/java/com/chinawyny/algorithm/Test.java
@@ -0,0 +1,8 @@
+package com.chinawyny.algorithm;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println(new String("a").substring(0, 0).length());
+    }
+}
